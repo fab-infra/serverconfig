2f030a336f3d638ed7fd5229cd76f2697b6d6e50
Fabien Crespel <fabien@crespel.net>
2024-04-22 23:09:35 +0200

    chore: use docker compose v2

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ TARGET_OS="${1:-rocky8}"
 
 # Run container
 pushd "$SCRIPT_DIR" >/dev/null 2>&1
-docker-compose build "$TARGET_OS" && docker-compose run --rm "$TARGET_OS" "${@:2}"
+docker compose build "$TARGET_OS" && docker compose run --rm "$TARGET_OS" "${@:2}"
 RET=$?
 popd >/dev/null 2>&1
 
